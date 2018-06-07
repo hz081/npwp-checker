37179cc127ee9487a42517badbbe5e8c15450bfb
Hantze <hantze.sudarma@gmail.com>
2018-06-07 22:07:34 +0700

    First Commit

---
+++ app/Http/Controllers/NpwpController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class NpwpController extends Controller
+{
+    public function check(Request $request, $npwp){
+
+        $urlNpwp    = 'https://sse3.pajak.go.id/cekWsDataWp';
+
+        $data       = array("npwp" => $npwp);
+        $dataJson   = json_encode($data);
+
+        $ch         = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $urlNpwp);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $dataJson);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+            'Content-Type: application/json',
+            'Cookie: TSPD_101='.$request->token
+        ));
+        $result     = curl_exec($ch);
+
+        return response()->json(json_decode($result), 200);
+    }
+}
---
+++ app/Http/Middleware/TokenMiddleware.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class TokenMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+
+        //init curl to get token
+        $url        = 'https://djponline.pajak.go.id/account/login';
+
+        $ch         = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        $html       = curl_exec($ch);
+
+        //extract html to get the data
+        $dom        = new \DOMDocument();
+        $dom->loadHTML($html);
+        $scripts    = $dom->getElementsByTagName('script');
+
+        //get token for future request
+        $script     = $scripts[0];
+        $nodeValue  = $script->nodeValue;
+        preg_match('/\window\.([a-zA-Z]{2})\s*=(.*?);/', $nodeValue, $matches);
+        preg_match('/"([^"]+)"/', $matches[0], $token);
+        $token      = $token[1];
+        $request->merge(compact('token'));
+
+        return $next($request);
+    }
+}
